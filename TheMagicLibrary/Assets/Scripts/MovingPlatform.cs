6160590b08c049fc77051e1ea636a6d8def530b2
ImpDesign <impdesign.bm@gmail.com>
2015-09-17 10:45:54 -0600

    New Project Creation
    Created new unity project and implemented basic folder structure.
    Scripts and basic sprites taken from Unity Tutorial.

---
+++ TheMagicLibrary/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour {
+
+	public Vector3 endPosition = Vector3.zero;
+	public float speed = 1;
+
+
+	private float timer = 0;
+	private Vector3 startPosition = Vector3.zero;
+
+	void Start () {
+		
+		startPosition = this.gameObject.transform.position;
+		endPosition = endPosition + startPosition;
+		
+		float distance = Vector3.Distance (startPosition, endPosition);
+		if (distance != 0) {
+			speed = speed/distance;
+		}
+	}
+	
+	// Update is called once per frame startPosition = Vector3.zero;
+	private bool outgoing = true;
+
+	// Use this for initialization
+	void Update () {
+
+		timer += Time.deltaTime * speed;
+
+		if (outgoing) {
+			this.transform.position = Vector3.Lerp (startPosition, endPosition, timer);
+			if(timer > 1) {
+				timer = 0;
+				outgoing = false;
+			}
+		} else {
+			this.transform.position = Vector3.Lerp (endPosition, startPosition, timer);
+			if(timer > 1){
+				timer = 0;
+				outgoing = true;
+			}
+		}
+
+	
+	}
+
+	void OnDrawGizmos () {
+
+		Gizmos.color = Color.red;
+		Gizmos.DrawLine (this.transform.position,endPosition + this.transform.position);
+	}
+}
---
+++ TheMagicLibrary/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,149 @@
+﻿using UnityEngine;
+using System.Collections;
+using Prime31;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public GameObject gameCamera;
+	public GameObject healthbar;
+	public GameObject gameOverPanel;
+    public GameObject invisiblePlatform;
+
+	Vector3 velocity = Vector3.zero;
+	public float speed = 5;
+	public float gravity = -1;
+	public float spring = 100;
+
+	public int health = 100;
+
+	private CharacterController2D _controller;
+	private AnimationController2D _animator;
+
+	private int currentHealth = 0;
+	private bool isAlive = true;
+
+	// Use this for initialization
+	void Start () {
+
+		_controller = gameObject.GetComponent<CharacterController2D>();
+		_animator = gameObject.GetComponent<AnimationController2D>();
+		gameCamera.gameObject.GetComponent<CameraFollow2D>().startCameraFollow();
+		currentHealth = health;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		velocity =  _controller.velocity;
+
+		if (_controller.isGrounded && (_controller.ground != null) && (_controller.ground.tag == "MovingPlatform")) {
+
+			this.transform.parent = _controller.ground.transform;
+
+		} 
+		else {
+
+			if( this.transform.parent != null){
+
+				this.transform.parent = null;
+
+			}
+		}
+
+		velocity.x = 0;
+
+		if (isAlive) {
+
+			if (Input.GetAxis ("Horizontal") < 0) {
+
+				velocity.x = -speed;
+				if (_controller.isGrounded) {
+					_animator.setAnimation ("Run");
+				}
+				_animator.setFacing ("Left");
+
+			} else if (Input.GetAxis ("Horizontal") > 0) {
+
+				velocity.x = speed;
+				if (_controller.isGrounded) {
+					_animator.setAnimation ("Run");
+				}
+				_animator.setFacing ("Right");
+
+			} else {
+
+				if (_controller.isGrounded) {
+					_animator.setAnimation ("Idle");
+				}
+
+			}
+
+			if (Input.GetAxis ("Jump") > 0 && _controller.isGrounded) {
+
+				velocity.y = Mathf.Sqrt (2f * spring * -gravity);
+				_animator.setAnimation ("Jump");
+
+			}
+		}
+
+		velocity.y += gravity;
+		_controller.move (velocity * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            invisiblePlatform.SetActive(true);
+        }
+        else
+        {
+            invisiblePlatform.SetActive(false);
+
+        }
+
+    }
+
+	void OnTriggerEnter2D(Collider2D col) {
+
+		if (col.tag == "KillZ") {
+
+			PlayerFallDeath ();
+
+		} 
+		else if (col.tag == "Damaging") {
+
+			PlayerDamage(25);
+
+		}
+
+	}
+
+	private void PlayerDeath(){
+
+		isAlive = false;
+		_animator.setAnimation("Death");
+		gameOverPanel.SetActive (true);
+
+	}
+
+	private void PlayerDamage(int damage) {
+
+		currentHealth -= damage;
+
+		float normalizedHealth = (float)currentHealth / (float)health;
+
+		healthbar.GetComponent<RectTransform> ().sizeDelta = new Vector2 (normalizedHealth * 256, 32);
+		if (currentHealth <= 0) {
+			PlayerDeath ();
+		}
+	}
+
+	private void PlayerFallDeath() {
+
+		currentHealth = 0;
+		healthbar.GetComponent<RectTransform> ().sizeDelta = new Vector2 (0 , 32);
+		gameCamera.gameObject.GetComponent<CameraFollow2D>().stopCameraFollow();
+		gameOverPanel.SetActive (true);
+
+	}
+}
