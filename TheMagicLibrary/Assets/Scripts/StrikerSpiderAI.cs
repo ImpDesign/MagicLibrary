23fefad9a5db406e6a3ae7d0d248abdc211815b2
ImpDesign <impdesign.bm@gmail.com>
2015-10-12 21:40:36 -0600

    More Spiders!
    Defined a "Striker Spider" as the one you have to get close to and can
    burn with a fire spell. Defined a "Timer Spider" as the invulnerable
    spider that you have to evade.

---
+++ TheMagicLibrary/Assets/Scripts/StrikerSpiderAI.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StrikerSpiderAI : MonoBehaviour {
+
+    public GameObject player;
+    public Vector3 range;
+    public float speed = 8;
+
+    private Vector3 startPosition;
+    private Vector3 endPosition;
+    private Vector3 lastSpot;
+    private float timer = 0;
+    private bool goingUp = false;
+
+    void Start()
+    {
+
+        startPosition = this.gameObject.transform.position;
+        endPosition = this.gameObject.transform.position;
+        lastSpot = this.gameObject.transform.position;
+
+        float distance = Vector3.Distance(startPosition, new Vector3(startPosition.x, startPosition.y - range.y));
+        speed = speed / distance;
+    }
+
+    void Update()
+    {
+
+        timer += Time.deltaTime;
+        endPosition.y = player.transform.position.y + 0.25f;
+
+        if (player.transform.position.x >= (startPosition.x - range.x) &&
+            player.transform.position.x <= (startPosition.x + range.x) &&
+            player.transform.position.y < startPosition.y &&
+            player.transform.position.y > (startPosition.y - range.y))
+        {
+            if (goingUp)
+            {
+                timer = 0;
+                lastSpot = this.transform.position;
+            }
+            this.transform.position = Vector3.Lerp(lastSpot, endPosition, timer);
+            lastSpot = this.gameObject.transform.position;
+            goingUp = false;
+        }
+        else
+        {
+            if (!goingUp)
+            {
+                timer = 0;
+                lastSpot = this.transform.position;
+            }
+            this.transform.position = Vector3.Lerp(lastSpot, startPosition, timer / 4);
+            goingUp = true;
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "FireBall")
+        {
+            Destroy(col.gameObject);
+            Destroy(this.gameObject);
+
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        Vector3 lineDraw = range;
+        lineDraw.y = -(range.y);
+        Gizmos.DrawLine(this.transform.position, lineDraw + this.transform.position);
+
+        lineDraw.x = -range.x;
+        Gizmos.DrawLine(this.transform.position, lineDraw + this.transform.position);
+    }
+
+}
---
+++ TheMagicLibrary/Assets/Scripts/TimerSpiderAI.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerSpiderAI : MonoBehaviour {
+
+    public float range;
+    public float speed = 10f;
+    public float pause = 1f;
+
+    private float timer = 0;
+    private float pauseTimer = 0;
+    private bool outgoing = true;
+    private Vector3 startPosition = Vector3.zero;
+    private Vector3 endPosition = Vector3.zero;
+
+    void Start()
+    {
+
+        startPosition = this.gameObject.transform.position;
+        endPosition = this.gameObject.transform.position + new Vector3(0, -range, 0);
+
+        float distance = Vector3.Distance(startPosition, new Vector3(startPosition.x, startPosition.y - range));
+        if (distance != 0)
+        {
+            speed = speed / distance;
+        }
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime * speed;
+
+        if (pauseTimer > 0)
+        {
+            pauseTimer -= Time.deltaTime;
+            timer = 0;
+        }
+        else if (outgoing)
+        {
+            this.transform.position = Vector3.Lerp(startPosition, endPosition, timer);
+            if (timer > 1)
+            {
+                timer = 0;
+                outgoing = false;
+                pauseTimer = pause;
+            }
+        }
+        else
+        {
+            this.transform.position = Vector3.Lerp(endPosition, startPosition, timer);
+            if (timer > 1)
+            {
+                timer = 0;
+                outgoing = true;
+                pauseTimer = pause;
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawLine(this.transform.position, new Vector3(0, -range - 1, 0) + this.transform.position);
+    }
+}
