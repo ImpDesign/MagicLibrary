0cdd6abb923557d4dedf6a8a3f48e66f16d578b7
ImpDesign <impdesign.bm@gmail.com>
2015-11-17 15:12:26 -0700

    Attempts at changing background
    Dark background not yet fiesable.

---
+++ TheMagicLibrary/Assets/Scripts/BackgroundMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundMovement : MonoBehaviour {
+
+    public float scrollSpeed = .1f;
+    public float tileSizeZ;
+    private GameObject player;
+
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+
+        if (GameObject.Find("Player") != null)
+        {
+            player = GameObject.Find("Player");
+        }
+        else
+        {
+            player = GameObject.Find("DarkPlayer");
+        }
+    }
+
+    void Update()
+    {
+        float newPosition = Mathf.Repeat((player.transform.position.x * scrollSpeed)%1, tileSizeZ);
+        transform.position = startPosition + Vector3.forward * newPosition;
+    }
+
+}
