174d08864f1cddb04d0f582335e18ee1ede19e94
ImpDesign <impdesign.bm@gmail.com>
2015-12-09 23:28:21 -0700

    Removed Movie
    MP4 was not working. MOV was too large couldn't be read. Kori will need
    to make a file that isn't 800+MB, more like 50MB.

---
+++ TheMagicLibrary/Assets/Scenes/Clips/Video.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (AudioSource))]
+
+public class Video : MonoBehaviour {
+
+    public MovieTexture movie;
+
+	void Start ()
+    {
+        GetComponent<Renderer>().material.mainTexture = movie as MovieTexture;
+        //GetComponent<AudioSource>() = movie.audioClip;
+        movie.Play();
+        GetComponent<AudioSource>().Play();
+	}
+
+	void Update ()
+    {
+	
+	}
+}
